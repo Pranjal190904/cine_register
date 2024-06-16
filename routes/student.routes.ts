cefee5bcb92b95097846a9d9409b53c5b7502b9b
rishisingh34 <singh34rishi@gmail.com>
2024-06-16 21:36:05 +0530

    feat: add login API endpoint for students

--- routes/student.routes.ts
+++ routes/student.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { registerStudent,verifyStudent } from "../controllers/student.controller";
+import { testController } from "../controllers/test.controller";   
 const router = Router();
 
 router.post("/register", registerStudent);
 router.post("/verify", verifyStudent);
+router.post("/login", testController.login);
 
 export default router;
