81af7ef88477826024f0ece31a3b6b93bb56f789
rishisingh34 <singh34rishi@gmail.com>
2024-06-20 17:25:07 +0530

    refactor: Remove RECAPTCHA_SECRET_KEY_LOGIN from env.config.ts and test.controller.ts

--- routes/test.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { testController } from "../controllers/test.controller";
-const router = Router();
-
-router.post("/login", testController.login);
-
-export default router;
